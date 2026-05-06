c9c596f950bdffaf1a037d33752dcad7bb63c349
SATADRU1 <satadrumondal1331@gmail.com>
2026-05-06 13:39:49 +0530

    login page is created successfully with prpper authentication

--- frontend/src/features/auth/pages/login.jsx
+++ frontend/src/features/auth/pages/login.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
 import '../auth.form.scss'
 import { Link } from 'react-router-dom'
+import { useAuth } from '../hooks/useAuth'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+    const { handleLogin , loading } = useAuth();
+    const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
+        await handleLogin({email, password})
+        navigate('/')
     };
+
+
+    if(loading) {
+        return (
+            <main>
+                <div className="form-container">
+                    <h1>Loading.....</h1>
+                </div>
+            </main>
+        )
+    }
   return (
     <main>
         <div className="form-container">
@@ -15,11 +36,15 @@ const Login = () => {
             <form onSubmit={handleSubmit}>
                 <div className="input-group">
                     <label htmlFor="email">Email</label>
-                    <input type="email" id="email" name="email" placeholder='Enter email address' />
+                    <input 
+                        onChange={(e) => setEmail(e.target.value)}
+                        type="email" id="email" name="email" placeholder='Enter email address' />
                 </div>
                 <div className="input-group">
                     <label htmlFor="password">Password</label>
-                    <input type="password" id="password" name="password" placeholder='Enter password' />
+                    <input 
+                        onChange={(e) => setPassword(e.target.value)}
+                        type="password" id="password" name="password" placeholder='Enter password' />
                 </div>
                 <button className='button primary-button'>Login</button>
             </form>
